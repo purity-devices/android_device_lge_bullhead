ae245692e337b5eed698050bfbae27e47262f6cb
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-11-18 15:45:00 -0500

    keymaster: Use TARGET_KEYMASTER_WAIT_FOR_QSEE
    Change-Id: I6a0ed8edc69a7e2c048c29606037b4e106a4c856

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -12,6 +12,9 @@ TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
+# Keymaster - Wait for qseecom to load
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
 # TWRP
 TW_THEME := portrait_hdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
