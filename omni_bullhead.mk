86ac11537ef80c438d91eb0175e585fd9fcfc26f
Jake Whatley <jacob.whatley91@gmail.com>
2016-10-04 14:55:39 -0400

    bullhead: generate Omni makefiles
    Change-Id: I8505502841456559f72f3c1b40cf8dce7ddb1828

---
+++ omni_bullhead.mk
@@ -0,0 +1,16 @@
+# Inherit base AOSP device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.0/NBD90W/3239497:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NBD90W 3239497 release-keys"
+
