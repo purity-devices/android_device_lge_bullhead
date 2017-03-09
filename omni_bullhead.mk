841256b41198868e5f9fb9402728985d1813cd9a
Jake Whatley <jacob.whatley91@gmail.com>
2017-03-08 22:58:54 -0500

    bullhead: N4F26T
    Change-Id: I13c25ac3e27533daa6b2f2af5449df7636e17e7d

--- omni_bullhead.mk
+++ omni_bullhead.mk
@@ -26,6 +26,6 @@ PRODUCT_MODEL := Nexus 5X
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/N4F26O/3582057:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26O 3582057 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/N4F26T/3687331:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26T 3687331 release-keys"
 
